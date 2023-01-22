7036cef61b89dc8c66aee8ca93c4194b638df2dc
Vroomfrondal <lionscantcode@gmail.com>
2023-01-21 19:55:04 -0600

    feat: mongoDB integration
    - added  mongoDB to host databases
    - created /api handler route to fetch server-side DB request
    -

---
+++ pages/api/new-meetup.ts
@@ -0,0 +1,30 @@
+// /api/new-meetup        --> URL
+// POST /api/new-meetup
+
+import { MongoClient } from 'mongodb'
+
+// Basic API route which will insert meetups in out DB
+async function handler(request: any, response: any) {
+    if (request.method === 'POST') {
+        const data = request.body
+        const { title, image, address, description } = data
+
+        try {
+            // This file isn't ever rendered client side anyway... but still keep it out of source
+            const client = await MongoClient.connect(process.env.MONGO_URL!)
+            const db = client.db()
+            const meetupsCollection = db.collection('LearnNextJs')
+            const result = await meetupsCollection.insertOne(data)
+
+            console.log(result)
+            client.close()
+
+            // Send HTTP Status after closing connection
+            response.status(201).json({ message: 'Meetup created!' })
+        } catch (err) {
+            console.log(err)
+        }
+    }
+}
+
+export default handler
--- pages/new-meetup/index.tsx
+++ pages/new-meetup/index.tsx
@@ -1,8 +1,10 @@
 import NewMeetupForm from '../../components/meetups/NewMeetupForm'
 
 export default function NewMeetupPage() {
-    const addMeetupHandler = (enteredMeetupData: any) => {
+    const addMeetupHandler = async (enteredMeetupData: Record<string, string>) => {
         console.log(enteredMeetupData)
+        // request to API route (internal api inside /api)
+        const response = await fetch('/api/new-meetup')
     }
 
     return (
